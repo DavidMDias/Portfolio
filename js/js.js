be8d73e35d11975102ae9be2930920690565a129
DavidMDias <31806029+DavidMDias@users.noreply.github.com>
2020-02-12 22:18:20 -0500

    Ajout d'un lien youtube

--- js/js.js
+++ js/js.js
@@ -8,7 +8,7 @@ function btnsVideo()
 	document.querySelector(".video4").addEventListener("mousedown", function(){
 		document.querySelector(".vide").style.display = "block";
 		document.querySelector(".fenetreVideo4").style.display = "block";
-		document.querySelector(".fenetreVideo4").getElementsByTagName("iframe")[0].src="https://www.youtube.com/embed/3CC2EnNHuN4?rel=0;";
+		document.querySelector(".fenetreVideo4").getElementsByTagName("iframe")[0].src="";
 	});
 	document.querySelector(".video3").addEventListener("mousedown", function(){
 		document.querySelector(".vide").style.display = "block";
@@ -18,7 +18,7 @@ function btnsVideo()
 	document.querySelector(".video2").addEventListener("mousedown", function(){
 		document.querySelector(".vide").style.display = "block";
 		document.querySelector(".fenetreVideo2").style.display = "block";
-		document.querySelector(".fenetreVideo2").getElementsByTagName("iframe")[0].src="https://www.youtube.com/embed/4gKsU_PfYtU?rel=0";
+		document.querySelector(".fenetreVideo2").getElementsByTagName("iframe")[0].src="https://www.youtube.com/embed/PtWWvatarlI?rel=0";
 	});
 	document.querySelector(".video1").addEventListener("mousedown", function(){
 		document.querySelector(".vide").style.display = "block";
